7098033c02488f4e4542b4c9bce91e8667ed2b1b
Yulia <yulia@gmail.com>
2022-05-08 21:36:42 +0300

    123

--- app.js
+++ app.js
@@ -29,13 +29,6 @@ const exampleLinks = [
 
 const groupsByData = lodash.groupBy(exampleLinks, 'data');
 
-function contains(where, what){
-    for(var i=0; i<what.length; i++){
-        if(where.indexOf(what[i]) == -1) return false;
-    }
-    return true;
-}
-
 let groupsByDataWithLevel = [];
 Object.values(groupsByData).forEach((group, groupIndex) => {
     groupsByDataWithLevel.push({
